05ed363d7e7ae161c61d592a2ab575c01f7fb1d6
romuloqaz <romulo.pereiracz@gmail.com>
2020-03-13 11:31:26 -0300

    insercao de usuario com POST

--- src/main/java/com/ifpb/workshopmongo/service/UserService.java
+++ src/main/java/com/ifpb/workshopmongo/service/UserService.java
@@ -1,6 +1,7 @@
 package com.ifpb.workshopmongo.service;
 
 import com.ifpb.workshopmongo.domain.User;
+import com.ifpb.workshopmongo.dto.UserDTO;
 import com.ifpb.workshopmongo.repository.UserRepository;
 import com.ifpb.workshopmongo.service.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj){
+        return repo.insert(obj);
+    }
+    
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
