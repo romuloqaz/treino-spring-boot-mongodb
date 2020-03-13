7f514b02c12518da47c7c1e2a1f2ec8db8b6d6ca
romuloqaz <romulo.pereiracz@gmail.com>
2020-03-13 14:42:27 -0300

    consulta simples com @query

--- src/main/java/com/ifpb/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/ifpb/workshopmongo/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package com.ifpb.workshopmongo.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.ifpb.workshopmongo.domain.Post;
@@ -10,6 +11,9 @@ import java.util.List;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
-    List<Post> findByTitleContainingIgnoreCase(String text);
+//consulta com titulo    List<Post> findByTitleContainingIgnoreCase(String text);
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }") // 0 primeiro parametro, i igonrar case sensitive
+    List<Post> searchTitle(String text);
 
 }
