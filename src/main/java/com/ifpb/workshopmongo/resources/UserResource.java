a81e278bc31e20cf53ce948b48b84b6bad4baa9e
romuloqaz <romulo.pereiracz@gmail.com>
2020-03-13 14:07:26 -0300

    endpoint para retornar posts do usuario

--- src/main/java/com/ifpb/workshopmongo/resources/UserResource.java
+++ src/main/java/com/ifpb/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.ifpb.workshopmongo.resources;
 
+import com.ifpb.workshopmongo.domain.Post;
 import com.ifpb.workshopmongo.domain.User;
 import com.ifpb.workshopmongo.dto.UserDTO;
 import com.ifpb.workshopmongo.service.UserService;
@@ -53,5 +54,10 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+    @RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+    public ResponseEntity <List<Post>> findPosts (@PathVariable String id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());//carrega os posts do usuario
     }
 }
